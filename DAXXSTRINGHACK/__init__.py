3d5a1f3eb3a4d53d6179e7d08bd7ff61ee413cea
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-05-11 09:43:36 +0530

    Update __init__.py

--- DAXXSTRINGHACK/__init__.py
+++ DAXXSTRINGHACK/__init__.py
@@ -39,7 +39,7 @@ async def DAXXSTRINGHACK():
     header = Table(show_header=True, header_style="bold green")
     header.add_column(LOG_TEXT)
     LOG.print(header)
-    LOG.print(f"[bold cyan]𝐌𝐑.𝐃𝐀𝐗𝐗")
+    LOG.print(f"[bold cyan]𝐌𝐑.𝐒𝐞𝐧𝐩𝐚𝐢")
     LOG.print("[bold yellow]𝐘𝐨𝐮𝐫 𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭.............")
     await app.start()    
     
